9d625868817e0c6f4a23a1ac645ee2ed6ced362c
Sergei Bykov <ss.bykoff@gmail.com>
2024-07-13 07:18:20 +0300

    перенес создание карточки в отдельный метод

--- app/src/main/java/ru/netology/markers/InterfaceImpl/MapObjectTapListenerImpl.kt
+++ app/src/main/java/ru/netology/markers/InterfaceImpl/MapObjectTapListenerImpl.kt
@@ -1,6 +1,7 @@
 package ru.netology.markers.InterfaceImpl
 
 import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.widget.PopupMenu
 import androidx.navigation.fragment.findNavController
 import com.yandex.mapkit.geometry.Point
@@ -9,6 +10,7 @@ import com.yandex.mapkit.map.MapObjectTapListener
 import ru.netology.markers.R
 import ru.netology.markers.activity.MapsFragment
 import ru.netology.markers.databinding.MapObjectCardBinding
+import ru.netology.markers.dto.LocalMapObject
 import ru.netology.markers.utils.DialogManager
 import ru.netology.markers.viewmodel.MapsVeiwModel
 
@@ -22,50 +24,52 @@ class MapObjectTapListenerImpl(
             val selectedMapObject = localMapObjects.find { it.id == mapObject.userData }
             if (selectedMapObject == null) return@observe
             val context = fragment.requireContext()
-
-            val card = MapObjectCardBinding.inflate(fragment.layoutInflater).apply {
-                name.text = context.getString(R.string.title_name, selectedMapObject.name)
-                descriptin.text = context.getString(
-                    R.string.card_description,
-                    selectedMapObject.description
-                )
-                point.text = context.getString(
-                    R.string.location,
-                    selectedMapObject.latitude.toFloat(),
-                    selectedMapObject.longitude.toFloat()
-                )
-                root.setOnClickListener {
-                    DialogManager.dismissDialog()
-                }
-                menu.setOnClickListener {
-                    PopupMenu(it.context, it).apply {
-                        inflate(R.menu.object_list_menu)
-                        setOnMenuItemClickListener { item ->
-                            when (item.itemId) {
-                                R.id.remove -> {
-                                    viewModel.removeById(selectedMapObject.id)
-                                    DialogManager.dismissDialog()
-                                    true
-                                }
-
-                                R.id.edit -> {
-                                    viewModel.edit(selectedMapObject)
-                                    fragment.findNavController().navigate(
-                                        R.id.action_mapsFragment_to_newMapObject
-                                    )
-                                    dismiss()
-                                    true
-                                }
-
-                                else -> false
-                            }
-                        }
-                    }.show()
-                }
-            }
-            DialogManager.showDialog(context, card.root)
+            DialogManager.showDialog(context, bind(context, selectedMapObject).root)
         }
         return true
 
     }
+
+     private fun bind(context: Context, localMapObject: LocalMapObject) : MapObjectCardBinding {
+         return MapObjectCardBinding.inflate(fragment.layoutInflater).apply {
+             name.text = context.getString(R.string.title_name, localMapObject.name)
+             descriptin.text = context.getString(
+                 R.string.card_description,
+                 localMapObject.description
+             )
+             point.text = context.getString(
+                 R.string.location,
+                 localMapObject.latitude.toFloat(),
+                 localMapObject.longitude.toFloat()
+             )
+             root.setOnClickListener {
+                 DialogManager.dismissDialog()
+             }
+             menu.setOnClickListener {
+                 PopupMenu(it.context, it).apply {
+                     inflate(R.menu.object_list_menu)
+                     setOnMenuItemClickListener { item ->
+                         when (item.itemId) {
+                             R.id.remove -> {
+                                 viewModel.removeById(localMapObject.id)
+                                 DialogManager.dismissDialog()
+                                 true
+                             }
+
+                             R.id.edit -> {
+                                 viewModel.edit(localMapObject)
+                                 fragment.findNavController().navigate(
+                                     R.id.action_mapsFragment_to_newMapObject
+                                 )
+                                 dismiss()
+                                 true
+                             }
+
+                             else -> false
+                         }
+                     }
+                 }.show()
+             }
+         }
+     }
 }
