9c39fd65c12ad90883ae064058b6e7432a84f19a
Sergei Bykov <ss.bykoff@gmail.com>
2024-07-25 11:09:39 +0300

    убрал баг с появлением информации по объекту после его перетаскивания

--- app/src/main/java/ru/netology/markers/InterfaceImpl/MapObjectTapListenerImpl.kt
+++ app/src/main/java/ru/netology/markers/InterfaceImpl/MapObjectTapListenerImpl.kt
@@ -20,57 +20,56 @@ class MapObjectTapListenerImpl(
 ) : MapObjectTapListener {
     @SuppressLint("StringFormatMatches")
     override fun onMapObjectTap(mapObject: MapObject, p1: Point): Boolean {
-        viewModel.data.observe(fragment.viewLifecycleOwner) { localMapObjects ->
-            val selectedMapObject = localMapObjects.find { it.id == mapObject.userData }
-            if (selectedMapObject == null) return@observe
-            val context = fragment.requireContext()
-            val card = bind(context, selectedMapObject)
-            DialogManager.showDialog(context, card.root)
-        }
+        val localMapObjects = viewModel.data.value
+        val selectedMapObject = localMapObjects?.find { it.id == mapObject.userData }
+        if (selectedMapObject == null) return false
+        val context = fragment.requireContext()
+        val card = bind(context, selectedMapObject)
+        DialogManager.showDialog(context, card.root)
         return true
 
     }
 
-     private fun bind(context: Context, localMapObject: LocalMapObject) : MapObjectCardBinding {
-         return MapObjectCardBinding.inflate(fragment.layoutInflater).apply {
-             name.text = context.getString(R.string.title_name, localMapObject.name)
-             descriptin.text = context.getString(
-                 R.string.card_description,
-                 localMapObject.description
-             )
-             point.text = context.getString(
-                 R.string.location,
-                 localMapObject.latitude.toFloat(),
-                 localMapObject.longitude.toFloat()
-             )
-             root.setOnClickListener {
-                 DialogManager.dismissDialog()
-             }
-             menu.setOnClickListener {
-                 PopupMenu(it.context, it).apply {
-                     inflate(R.menu.object_list_menu)
-                     setOnMenuItemClickListener { item ->
-                         when (item.itemId) {
-                             R.id.remove -> {
-                                 viewModel.removeById(localMapObject.id)
-                                 DialogManager.dismissDialog()
-                                 true
-                             }
+    private fun bind(context: Context, localMapObject: LocalMapObject): MapObjectCardBinding {
+        return MapObjectCardBinding.inflate(fragment.layoutInflater).apply {
+            name.text = context.getString(R.string.title_name, localMapObject.name)
+            descriptin.text = context.getString(
+                R.string.card_description,
+                localMapObject.description
+            )
+            point.text = context.getString(
+                R.string.location,
+                localMapObject.latitude.toFloat(),
+                localMapObject.longitude.toFloat()
+            )
+            root.setOnClickListener {
+                DialogManager.dismissDialog()
+            }
+            menu.setOnClickListener {
+                PopupMenu(it.context, it).apply {
+                    inflate(R.menu.object_list_menu)
+                    setOnMenuItemClickListener { item ->
+                        when (item.itemId) {
+                            R.id.remove -> {
+                                viewModel.removeById(localMapObject.id)
+                                DialogManager.dismissDialog()
+                                true
+                            }
 
-                             R.id.edit -> {
-                                 viewModel.edit(localMapObject)
-                                 fragment.findNavController().navigate(
-                                     R.id.action_mapsFragment_to_newMapObject
-                                 )
-                                 dismiss()
-                                 true
-                             }
+                            R.id.edit -> {
+                                viewModel.edit(localMapObject)
+                                fragment.findNavController().navigate(
+                                    R.id.action_mapsFragment_to_newMapObject
+                                )
+                                dismiss()
+                                true
+                            }
 
-                             else -> false
-                         }
-                     }
-                 }.show()
-             }
-         }
-     }
+                            else -> false
+                        }
+                    }
+                }.show()
+            }
+        }
+    }
 }
